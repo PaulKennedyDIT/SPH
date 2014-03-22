585fe5ea735ee9bab8b623746e9e9689d5b53fc9
PaulKennedyDIT <c10319445@mydit.ie>
2014-03-22 21:56:18 +0000

    Added Poly6 Commenting

--- Assets/Code/SPH/Poly6.cs
+++ Assets/Code/SPH/Poly6.cs
@@ -2,13 +2,19 @@
 using System.Collections;
 using System;
 
+/* Poly 6 Smoothing Kernel
+ * Contains a definition of the Poly6 Smoothing kernel including the Gradient and Laplacian of the Poly6 function.
+ */
 public class Poly6 : SmoothingKernel
 {
 	public Poly6(double SmoothingLength)
 	{
 		this.SmoothingLengthH = SmoothingLength;
 	}
-	
+
+	//	As defined by Mathias Muller et al (2003) - poly6(r,h) = if 0 ≤ r ≤ h  then 315/64*π*(h^9) otherwise 0.
+	//	where r = Square Magnitude of Distance Vector.
+	// 		  h = Smoothing Length
 	public override double Calculate(ref Vector3 distance)
 	{
 		this.Scaling = (315.0f/ (64.0f * Math.PI * (float)Math.Pow(SmoothingLength,9.0f)));
@@ -23,7 +29,11 @@ public class Poly6 : SmoothingKernel
 
 		return Scaling * (h2minusr2 * h2minusr2 * h2minusr2);
 	}
-	
+
+	// Gradient as derived by Stefan Auer in the paper Realtime particle-based fluid simulation
+	// ∇Poly6(r,h) = -r * (945/ 32 * π * h^9) * (h^2 - r^2)^2
+	//				 where r = Square Magnitude of Distance Vector.
+	//					   h = Smoothing Length
 	public override Vector3 CalculateGradient (ref Vector3 distance)
 	{
 		this.Scaling = (945.0f / (32.0f * Mathf.PI * (float)Math.Pow (SmoothingLength, 9.0f)));
@@ -39,7 +49,9 @@ public class Poly6 : SmoothingKernel
 
 		return new Vector3(-distance.x * (float)scalar, -distance.y * (float)scalar,-distance.z * (float)scalar);
 	}
-	
+
+	// Laplacian as derived by Stefan Auer in the paper Realtime particle-based fluid simulation
+	//	∇^2 Poly6(r,h) = (945/ 8 * π * h^9) * (h^2 - r^2) * ( r^2 - 3/4 * (h^2 - r^2))
 	public override double CalculateLaplacian(ref Vector3 distance)
 	{
 		this.Scaling = (945.0f)/(8.0f * Math.PI * (float)Math.Pow(SmoothingLength,9.0f));
