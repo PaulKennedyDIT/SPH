6f8cea95120574f7608e22317a4118404a3503cc
PaulKennedyDIT <c10319445@mydit.ie>
2014-03-23 03:54:38 +0000

    Added GameManager Comments

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/* GameManager implemented as a Singleton instance.
+*  Contains globally accessible variables including the Particle Resolution (Particle Number) and the Particle Size.
+*  Retains the assignment of the GameManagers fields throughout all of the scenes loaded at run time.
+*/
 public class GameManager : MonoBehaviour
 {
 	public static GameManager manager;
